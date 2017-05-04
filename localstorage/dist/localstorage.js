3ae5f6381e800c74da3183bcc8be26acfeddf0cf
pfan <768065158@qq.com>
2017-05-04 19:45:44 +0800

    修改localstorage.js，增加cmd支持

--- localstorage/dist/localstorage.js
+++ localstorage/dist/localstorage.js
@@ -9,6 +9,10 @@
 (function (root, factory) {
     if (typeof define === 'function' && define.amd) {
         define([], factory);
+    } else if (typeof define === 'function' && define.cmd) {
+        define(function(require, exports, module){
+            module.exports = factory()
+        });
     } else if (typeof exports === 'object') {
         module.exports = factory();
     } else {
